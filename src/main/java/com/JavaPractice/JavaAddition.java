cbe65112b07902d5245a35bff302be2b7fedac8b
Srikanth <srikanthh994@gmail.com>
2021-09-22 21:22:23 +0530

    Added: Addition of two numbers

---
+++ src/main/java/com/JavaPractice/JavaAddition.java
@@ -0,0 +1,15 @@
+package com.JavaPractice;
+
+public class JavaAddition {
+
+	public static void main(String[] args) {
+		
+		int num1 = 20;
+		int num2 = 15;
+        int sum;
+		sum = num1 + num2;
+				
+		System.out.println("sum of these number" +sum);
+		
+	}
+}
