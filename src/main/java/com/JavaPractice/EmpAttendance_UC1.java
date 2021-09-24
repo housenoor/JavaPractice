3756400cbd8154cfa3423c62e19bce7caafbbac5
Srikanth <srikanthh994@gmail.com>
2021-09-24 14:39:30 +0530

    Added:EmpAttendance

---
+++ src/main/java/com/JavaPractice/EmpAttendance_UC1.java
@@ -0,0 +1,19 @@
+package com.JavaPractice;
+
+public class EmpAttendance_UC1 {
+
+	    public static void main(String[] args){
+	        System.out.println("Welcome to Java");
+	        System.out.println("Welcome to Bridgelabz EmployeeWageBuilder");
+	//constant
+	        int IS_PRESENT = 1;
+
+	        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+	        if (empCheck == IS_PRESENT)
+	            System.out.println(" Employee is Present");
+	        else
+	            System.out.println(" Employee is Absent");
+	    }
+	}
+
+
