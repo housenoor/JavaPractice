75914ec9265e4831f8a12b7247786c5cdccfe503
Srikanth <srikanthh994@gmail.com>
2021-09-26 09:03:16 +0530

    Added:Line Comparison project

---
+++ main/java/com/JavaPractice/EmpAttendance.java
@@ -0,0 +1,17 @@
+package com.JavaPractice;
+
+public class EmpAttendance {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+        int IS_PRESENT = 1;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println(" Employee is Present");
+        else
+            System.out.println(" Employee is Absent");
+    }
+}
+
